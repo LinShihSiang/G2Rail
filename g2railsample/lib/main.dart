4dd0662514a0d5ee6043ab26ddd70b34198eae1e
mike.lin <mike.lin@onelab.tw>
2025-09-12 17:37:24 +0800

    Modify
    1. add api button
    2. add api spec

--- g2railsample/lib/main.dart
+++ g2railsample/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:g2railsample/g2rail_api_client.dart';
 import 'package:http/http.dart';
@@ -17,11 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'G2Rail API Sample',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'G2Rail API Sample'),
     );
   }
 }
@@ -36,37 +37,57 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String _apiResponse = 'No API response yet';
   Client baseClient() {
-    HttpClient httpClient = HttpClient();
-    httpClient.badCertificateCallback =
-        (X509Certificate cert, String host, int port) {
-          return true;
-        };
-    Client c = IOClient(httpClient);
-    return c;
+    if (kIsWeb) {
+      // For web platforms, use the default HTTP client
+      return Client();
+    } else {
+      // For mobile/desktop platforms, use IOClient with certificate bypass
+      HttpClient httpClient = HttpClient();
+      httpClient.badCertificateCallback =
+          (X509Certificate cert, String host, int port) {
+            return true;
+          };
+      return IOClient(httpClient);
+    }
   }
 
   void _actAPI() async {
-    String baseUrl = "http://alpha-api.g2rail.com";
-    var gac = GrailApiClient(
-      httpClient: baseClient(),
-      baseUrl: baseUrl,
-      apiKey: "<API-Key>",
-      secret: "<API-Secret>",
-    );
-    var rtn = await gac.getSolutions(
-      "Frankfurt",
-      "Berlin",
-      DateFormat(
-        "yyyy-MM-dd",
-      ).format(DateTime.now().add(const Duration(days: 7))),
-      "08:00",
-      1,
-      0,
-      0,
-      0,
-      0,
-    );
+    setState(() {
+      _apiResponse = 'Loading...';
+    });
+    
+    try {
+      String baseUrl = "http://alpha-api.g2rail.com";
+      var gac = GrailApiClient(
+        httpClient: baseClient(),
+        baseUrl: baseUrl,
+        apiKey: "fa656e6b99d64f309d72d6a8e7284953",
+        secret: "9a52b1f7-7c96-4305-8569-1016a55048bc",
+      );
+      var rtn = await gac.getSolutions(
+        "CT_LV7D4WNOK",
+        "CT_60Y990YWR",
+        DateFormat(
+          "yyyy-MM-dd",
+        ).format(DateTime.now().toUtc().add(const Duration(days: 0))),
+        "08:00",
+        1,
+        0,
+        0,
+        0,
+        0,
+      );
+      
+      setState(() {
+        _apiResponse = rtn.toString();
+      });
+    } catch (e) {
+      setState(() {
+        _apiResponse = 'Error: ${e.toString()}';
+      });
+    }
   }
 
   @override
@@ -79,7 +100,47 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[const Text('You have pushed the button')],
+          children: <Widget>[
+            const Text(
+              'G2Rail API Sample',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 20),
+            const Text('Test G2Rail API functionality'),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: _actAPI,
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                textStyle: const TextStyle(fontSize: 16),
+              ),
+              child: const Text('Test G2Rail API'),
+            ),
+            const SizedBox(height: 30),
+            const Text(
+              'API Response:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16),
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.grey[100],
+                  ),
+                  child: Text(
+                    _apiResponse,
+                    style: const TextStyle(fontSize: 12, fontFamily: 'monospace'),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
